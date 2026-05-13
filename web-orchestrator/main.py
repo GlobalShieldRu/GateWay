4c37b9757cff2e6cd8dd8826f375999d7b765a28
Alexandr <cherkashin@infsys.ru>
2026-05-13 20:02:45 +0300

    chore: release v1.13.3

--- web-orchestrator/main.py
+++ web-orchestrator/main.py
@@ -21,7 +21,7 @@ from fastapi.responses import FileResponse, JSONResponse, StreamingResponse
 from starlette.middleware.base import BaseHTTPMiddleware
 from pydantic import BaseModel
 
-GSG_VERSION = "1.13.2"
+GSG_VERSION = "1.13.3"
 _start_time = time.time()
 
 app = FastAPI(title="GSG Smart Gateway API")
